5886185597a1b435693fd62a9f8e511099a3c7c9
academiaseul <hola.academiaseul@gmail.com>
2026-06-16 06:31:19 -0400

    Update Google Analytics

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -4,7 +4,7 @@ import Script from 'next/script';
 import { useEffect } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 
-const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || 'G-8LFDEWXQWS';
 
 declare global {
   interface Window {
